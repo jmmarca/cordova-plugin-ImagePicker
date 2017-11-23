121a9a468690764d6d60399d009fc81bf05fe3d0
jmmarca <jmmarca@gmail.com>
2017-11-23 14:33:41 -0200

    Delete compile-android.sh

--- compile-android.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-git add -A && git commit -m "Correção Geral"
-git push
-cd ../midgard/packages/midgard-app-mobile/src/mobile
-npx phonegap plugin rm cordova-plugin-ImagePicker --save
-npx phonegap plugin add https://github.com/jmmarca/cordova-plugin-ImagePicker.git --save
-cordova build --device
-#cd ../../
-#npm run android

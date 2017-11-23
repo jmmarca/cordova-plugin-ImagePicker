feb040ac154a7fe056f98f48e6d78811f779b063
Jean Michél Marca <jean.marca@wssim.com.br>
2017-11-23 14:29:37 -0200

    Correção Geral

--- compile-android.sh
+++ compile-android.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 git add -A && git commit -m "Correção Geral"
 git push
-cd ..\midgard\packages\midgard-app-mobile\src\mobile
+cd ../midgard/packages/midgard-app-mobile/src/mobile
 npx phonegap plugin rm cordova-plugin-ImagePicker --save
 npx phonegap plugin add https://github.com/jmmarca/cordova-plugin-ImagePicker.git --save
 cordova build --device
